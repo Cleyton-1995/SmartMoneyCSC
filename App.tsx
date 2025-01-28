ff8e8ee360f661a21a29b556031003acac1d911d
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2025-01-28 15:54:45 -0300

    chore: add news dependencies

--- App.tsx
+++ App.tsx
@@ -1,22 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Main } from './src/screens/Main';
-import { NewEntry } from './src/screens/NewEntry';
-import { Report } from './src/screens/Report';
+import { Main } from "./src/pages/Main";
+import { NavigationContainer } from "@react-navigation/native";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NewEntry } from "./src/screens/NewEntry";
+import { Report } from "./src/screens/Report";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      {/* <Main/> */}
-      {/* <NewEntry/> */}
-      <Report/>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{ headerShown: false }}
+        initialRouteName="Main"
+      >
+        <Stack.Screen name="Main" component={Main} />
+        <Stack.Screen name="NewEntry" component={NewEntry} />
+        <Stack.Screen name="Report" component={Report} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
